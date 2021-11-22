d88a2eac832b825e2159439e862a0d4e0639b7fa
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-22 14:32:02 +0100

    How to add nested form(address-form) implementing ControlValueAccessor and OnDestroy to be part of the form step 1o be validaed

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -13,23 +13,59 @@ import {noop, Subscription} from 'rxjs';
 @Component({
   selector: 'address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
-})
-export class AddressFormComponent {
-
-    @Input()
-    legend:string;
-
-    form: FormGroup = this.fb.group({
-        addressLine1: [null, [Validators.required]],
-        addressLine2: [null, [Validators.required]],
-        zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
-    });
-
-    constructor(private fb: FormBuilder) {
+  styleUrls: ['./address-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: AddressFormComponent
     }
+  ]
+})
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
 
+  @Input()
+  legend:string;
+
+  onTouched = () => {};
+
+  onChangeSub: Subscription;
+
+  form: FormGroup = this.fb.group({
+      addressLine1: [null, [Validators.required]],
+      addressLine2: [null, [Validators.required]],
+      zipCode: [null, [Validators.required]],
+      city: [null, [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  writeValue(value: any): void {
+    if (value) {
+      this.form.setValue(value);
+    }
+  }
+
+  ngOnDestroy() {
+    this.onChangeSub.unsubscribe();
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChangeSub = this.form.valueChanges.subscribe(onChange);
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(disable: boolean) {
+    if (disable) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+  }
 }
 
 
