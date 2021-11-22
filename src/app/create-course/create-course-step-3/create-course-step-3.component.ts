e14cac1f42557d415e8d8b438de6e64ff0f3c6e6
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-22 16:36:03 +0100

    Array form implemented in ts

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, Validators} from '@angular/forms';
 
 
 @Component({
@@ -10,8 +10,26 @@ import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateCourseStep3Component {
 
   form = this.fb.group({
-
+    lessons: this.fb.array([])
   });
 
   constructor(private fb: FormBuilder) {}
+
+  get lessons() {
+     return this.form.controls["lessons"] as FormArray;
+  }
+
+  addLesson() {
+
+    const lessonForm = this.fb.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required]
+    });
+
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
 }
