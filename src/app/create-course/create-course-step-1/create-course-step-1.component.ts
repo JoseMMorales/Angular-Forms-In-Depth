fdf4cfe5c21a7adfc52f7a93c3cc3974afd839ca
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-22 09:25:40 +0100

    Create course-title validator to make async call to API for checking if name of the course is being used before in DDBB

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CoursesService } from '../../services/courses.service';
+import { courseTitleValidator } from '../../validators/course-title.validator';
 
 @Component({
   selector: 'create-course-step-1',
@@ -9,17 +11,25 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class CreateCourseStep1Component implements OnInit {
 
   form = this.fb.group({
-    title: ['', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(60)
-    ]]
+    title: ['', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(60)
+      ],
+      asyncValidators: [courseTitleValidator(this.courses)],
+      updateOn: 'blur'
+    }]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private courses: CoursesService ) {}
 
   ngOnInit() {
 
+  }
+
+  get courseTitle() {
+    return this.form.controls['title'];
   }
 
 }
